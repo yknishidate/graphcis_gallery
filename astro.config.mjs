9bca10529dd54104d9329c60c802a8b1e26fbb86
Yuki Nishidate <yuki18425@gmail.com>
2025-03-10 01:51:16 +0900

    fix: add trailing slash to base URL in Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://yknishidate.github.io',
-  base: '/graphics_gallery',
+  base: '/graphics_gallery/',
   devToolbar: {
     enabled: false
   }
