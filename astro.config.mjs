acd29b05adce0d571e8dbcaf10c7d938b21686b3
Yuki Nishidate <yuki18425@gmail.com>
2025-03-10 01:31:07 +0900

    refactor: update README and configuration for Graphics Gallery project

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,9 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://yknishidate.github.io',
+  base: '/graphics_gallery',
   devToolbar: {
     enabled: false
-  },
-  site: 'https://yknishidate.github.io',
-  base: '/graphcis_gallery',
+  }
 });
